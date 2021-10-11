fdadec9e15a05eb5e3e5fc5da4bee8e7e39012b5
yanet48 <yanet48@hotmail.com>
2021-10-11 13:33:36 -0500

    Proyecto Inicial

---
+++ resources/views/workers/results/load.blade.php
@@ -0,0 +1,10 @@
+<!--Crear un resultado específico de un resultadod e un trabajador-->
+@extends("layouts.template")
+
+@section("contenido") 
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+           
+    </div>
+
+    
+@endsection
