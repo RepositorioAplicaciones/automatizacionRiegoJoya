d9a198a1304f8e3ab3daeee29b6cf500dfd39bb8
yanet48 <yanet48@hotmail.com>
2021-10-13 06:59:47 -0500

    v2

--- app/Http/Controllers/ZonesController.php
+++ app/Http/Controllers/ZonesController.php
@@ -10,10 +10,11 @@ use \App\Models\Roster;
 use \App\Models\Area;
 use \App\Models\Worker;
 use \App\Models\Result;
+use \App\Models\Zone;
 
-//Controlador para la gestion de los trabajadores
+//Controlador para la gestion de los Zonas
 
-class WorkersController extends Controller
+class ZonesController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -22,8 +23,8 @@ class WorkersController extends Controller
      */
     public function index()
     {        
-        $trabajadores=Worker::all();        
-        return view("workers.index", compact("trabajadores"));           
+        $zonas=Zone::all();        
+        return view("zones.index", compact("zonas"));           
     }
 
     /**
@@ -34,10 +35,8 @@ class WorkersController extends Controller
     public function create()
     {
         Auth::user()->authorizeRoles(['user', 'administrador', 'operador']);        
-        $areas = Area::pluck('name', 'id');
-        $rosters = Roster::pluck('name', 'id');
-        $sex = array('hombre' => 'hombre', 'mujer' => 'mujer', 'otro' => 'otro');        
-        return view('workers.create', compact("areas", "rosters", "sex"));
+       
+        return view('zones.create');
 
     }
 
@@ -50,19 +49,11 @@ class WorkersController extends Controller
     public function store(Request $request)
     {
         Auth::user()->authorizeRoles(['user', 'administrador', 'operador']);
-        $worker = new Worker();        
-        $worker ->name = $request->name;
-        $worker ->age = $request->age;
-        $worker ->sex = $request->sex;
-        $worker ->DNI = $request->DNI;
-        $worker ->area_id = $request->area_id;
-        $worker ->roster_id = $request->roster_id;
-        $worker ->fecha_subida = $request->fecha_subida;
-        $worker ->fecha_bajada = $request->fecha_bajada;        
-
-        $worker->save();        
-
-        return redirect("/workers");
+        $zone = new Zone();        
+        $zone ->name = $request->name;
+        $zone ->description = $request->description;
+        $zone->save();        
+        return redirect("/zones");
     }
 
     /**
@@ -74,8 +65,8 @@ class WorkersController extends Controller
     public function show($id)
     {
         Auth::user()->authorizeRoles(['user', 'administrador', 'operador']);
-        $trabajador = Worker::findOrFail($id);
-        return view("workers.view", compact("trabajador"));        
+        $zonas = Zone::findOrFail($id);
+        return view("zonas.view", compact("zonas"));        
     }
 
     /**
@@ -87,11 +78,8 @@ class WorkersController extends Controller
     public function edit($id)
     {
         Auth::user()->authorizeRoles(['user', 'administrador', 'operador']);        
-        $trabajador = Worker::findOrFail($id);
-        $areas = Area::pluck('name', 'id');
-        $rosters = Roster::pluck('name', 'id'); 
-        $sex = array('hombre' => 'hombre', 'mujer' => 'mujer', 'otro' => 'otro');  
-        return view("workers.edit", compact("trabajador", "areas", "rosters", "sex")); 
+        $zonas = Zone::findOrFail($id);       
+        return view("zones.edit"); 
     }
 
     /**
@@ -104,19 +92,13 @@ class WorkersController extends Controller
     public function update(Request $request, $id)
     {
         Auth::user()->authorizeRoles(['user', 'administrador', 'operador']);
-        $trabajador = Worker::findOrFail($id);        
-        $trabajador ->name = $request->name;
-        $trabajador ->age = $request->age;
-        $trabajador ->sex = $request->sex;
-        $trabajador ->DNI = $request->DNI;
-        $trabajador ->area_id = $request->area_id;
-        $trabajador ->roster_id = $request->roster_id;
-        $trabajador ->fecha_subida = $request->fecha_subida;
-        $trabajador ->fecha_bajada = $request->fecha_bajada;
-        
-        $trabajador->update();
+        $zonas = Zone::findOrFail($id);        
+        $zonas ->name = $request->name;
+        $zonas ->description = $request->description;
+       
+        $zonas->update();
       
-        return redirect("/workers");
+        return redirect("/zones");
     }
 
     /**
@@ -128,8 +110,8 @@ class WorkersController extends Controller
     public function destroy($id)
     {        
         Auth::user()->authorizeRoles(['user', 'administrador', 'operador']);
-        $trabajador = Worker::findOrFail($id);
-        $trabajador->results()->delete();
-        $trabajador ->delete(); 
+        $zonas = Zone::findOrFail($id);
+        $zonas->results()->delete();
+        $zonas ->delete(); 
     }
 }
--- resources/views/zones/create.blade.php
+++ resources/views/zones/create.blade.php
@@ -3,10 +3,10 @@
 
 @section("contenido") 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Crear Trabajador x</h1>           
+        <h1 class="h2">Crear Zonas</h1>           
     </div>
   
-    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\WorkersController@store']) !!}
+    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\ZonesController@store']) !!}
         <table>
             <tr>
                 <td>{!! Form::label('name', 'Nombre') !!} </td>
@@ -14,42 +14,14 @@
             </tr>
 
             <tr>
-                <td>{!! Form::label('age', 'Edad') !!} </td>
-                <td>{!! Form::number('age', null, ['class' => 'form-control', 'required']) !!} </td>
+                <td>{!! Form::label('description', 'Descripción') !!} </td>
+                <td>{!! Form::text('description', null, ['class' => 'form-control', 'required']) !!} </td>
             </tr>            
 
-            <tr>
-                <td>{!! Form::label('sex', 'Sexo') !!} </td>
-                <td>{!! Form::select('sex', $sex, null, ['class' => 'form-control']) !!}</td>
-            </tr>
+           
 
             <tr>
-                <td>{!! Form::label('DNI', 'DNI') !!} </td>
-                <td>{!! Form::number('DNI', null, ['class' => 'form-control', 'required' , 'max' => 99999999 ] ) !!} </td>
-            </tr>  
-
-            <tr>
-                <td>{!! Form::label('area_id', 'Areas') !!} </td>
-                <td>{!! Form::select('area_id', $areas, null, ['class' => 'form-control']) !!}</td>
-            </tr>  
-
-            <tr>
-                <td>{!! Form::label('roster_id', 'Roster') !!} </td>
-                <td>{!! Form::select('roster_id', $rosters, null, ['class' => 'form-control']) !!}</td>
-            </tr>  
-
-            <tr>
-                <td>{!! Form::label('fecha_subida', 'Fecha de Subida') !!} </td>
-                <td>{!! Form::date('fecha_subida', null, ['class' => 'form-control', 'required']) !!} </td>
-            </tr>  
-
-            <tr>
-                <td>{!! Form::label('fecha_bajada', 'Fecha de Bajada') !!} </td>
-                <td>{!! Form::date('fecha_bajada', null, ['class' => 'form-control', 'required']) !!} </td>
-            </tr>  
-
-            <tr>
-                <td >{!! Form::submit('Crear Trabajador', ['class' => 'btn btn-primary' ]) !!} </td>
+                <td >{!! Form::submit('Crear Zona', ['class' => 'btn btn-primary' ]) !!} </td>
                 <td>{!! Form::reset('Borrar', ['class' => 'btn btn-danger' ]) !!} </td>
             </tr>            
 
